635688a5658c8ff0151c04f0cac3b0543e4b1f38
Luiz Henrique <120956095+LuizSilva28@users.noreply.github.com>
2024-04-19 04:51:03 -0300

    Funcionalindade de marcadao e alteracao da vez

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
-//Resgatando os nomes dos jogadores
 
-const btn = document.querySelector('.salvarNamePlayers');
 
-btn.addEventListener('click', function (e) {
+var bntIniciar = document.querySelector(".iniciarJogo");
+bntIniciar.addEventListener("click", function (e){
     e.preventDefault();
-    const player1 = document.querySelector('#player1').value;
+    const player1 = document.getElementById('player1').value;
     const player2 = document.getElementById('player2').value;
-    console.log(player1, player2)
+    marcadorDeJogada(player1, player2)
+    bntIniciar.setAttribute('disabled','true');
+});
 
-    jogadorDaVez(player1);
-
-})
-
-
-// Imprimindo na tela o nome do jogador da vez 
-
-function jogadorDaVez (player){
-    const jogadorDaVez = document.getElementById('jogadorDaVez');
-
-    jogadorDaVez.value = player;
-
-    console.log(jogadorDaVez.value);
+function jogadorDaVez(player){
+    var jogador = document.querySelector("#jogadorDaVez");
+    jogador.value = player;
+    return jogador.value;
 }
 
-document.querySelectorAll('.bnt').forEach(function (bntClicado) {
-    bntClicado.addEventListener('click', function () {
-        console.log(bntClicado.value);
-    })})
+  
+
+function marcadorDeJogada (player1, player2){
+    jogadorDaVez(player1)
+    var playerQueJogou = jogadorDaVez(player1)
+    i=1;
+    console.log(`Rodada ${i}: ` + playerQueJogou);
+    
+
+    document.querySelectorAll('.bnt').forEach(function (bntClicado){
+        bntClicado.addEventListener('click', function(){
+            console.log('Botão clicado com sucesso!')
+
+            
+
+            if (playerQueJogou === player1){
+
+            console.log(`Rodada ${i}: ` + playerQueJogou)
+            bntClicado.innerHTML = " X ";
+            jogadorDaVez(player2)
+            console.log('Jogador da vez agora é: '+ jogadorDaVez(player2))
+            playerQueJogou = jogadorDaVez(player2)
+            i++
+
+        } else if (playerQueJogou === player2){
+            console.log(`Rodada ${i}: ` + playerQueJogou)
+            bntClicado.innerHTML = " O ";
+            jogadorDaVez(player1)
+            console.log('Jogador da vez agora é: '+ jogadorDaVez(player1))
+            playerQueJogou = jogadorDaVez(player1)
+            i++
+
+        }
+        bntClicado.setAttribute('disabled','true');
+           
+        
+        }
+        )
+    })
+    
+}
+

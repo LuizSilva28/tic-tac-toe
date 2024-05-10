53106c4dd45b646f85aaa65ecef4fdf0bae0efef
Luiz Henrique <120956095+LuizSilva28@users.noreply.github.com>
2024-05-10 05:17:19 -0300

    correcoes de bloqueio de tabela e na declaracao de empate

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const inputs = document.querySelectorAll(".player").forEach(function (input) {
     e.preventDefault();
     input.setAttribute('maxLength', '10');
     input.style.outline = 'none';
-
   });
 });
-
 const bntIniciar = document.querySelector(".iniciarJogo");
 bntIniciar.addEventListener("click", function (e){
     e.preventDefault();
@@ -16,7 +14,6 @@ bntIniciar.addEventListener("click", function (e){
     bntIniciar.setAttribute('disabled','true');
     bntIniciar.id= 'blockIniciarJogo';
 });
-
 function jogadorDaVez(player){
     var jogador = document.querySelector("#jogadorDaVez");
     jogador.value = player;
@@ -24,7 +21,6 @@ function jogadorDaVez(player){
 }
 
 const div = document.getElementById('telaDaJogada');
-
 const labelTelaDaJogada = document.createElement("label");
 labelTelaDaJogada.htmlFor = 'jogadorDaVez'
 labelTelaDaJogada.innerHTML = "Jogador da vez: ";
@@ -42,14 +38,8 @@ inputTelaDaJogada.disabled = true;
 inputTelaDaJogada.placeholder = "...";
 inputTelaDaJogada.classList = 'jogadorDaVez';
 
-
 div.appendChild(inputTelaDaJogada);
-
-
-
-
 // CONSTRUINDO A TEBELA 
-
 const tabela = document.querySelector('#tabela');
  
 let a = 0, b = 0, c = 0;    
@@ -58,16 +48,14 @@ for (i = 0; i < 3; i++) {
     const li = document.createElement('li');
     li.classList.add('linha');
     li.style.listStyleType = 'none';
-    
 
     for (j = 0; j < 3; j++) {
-        
-    const area = document.createElement('button');
-    area.classList.add('bnt');
-    area.id = `areaDoVencedor-${a}`
-    area.setAttribute('placeholder', ' X ')
-    li.appendChild(area);
-    a++
+        const area = document.createElement('button');
+        area.classList.add('bnt');
+        area.id = `areaDoVencedor-${a}`
+        area.setAttribute('placeholder', ' X ')
+        li.appendChild(area);
+        a++
     }
     tabela.appendChild(li);
 }
@@ -76,49 +64,35 @@ function marcadorDeJogada (player1, player2){
     var playerQueJogou = jogadorDaVez(player1)
     i=1;
     console.log(`Rodada ${i}: ` + playerQueJogou);
-    
-
     document.querySelectorAll('.bnt').forEach(function (bntClicado){
-        
+
         bntClicado.addEventListener('click', function(){           
-
             if (playerQueJogou === player1){
-            bntClicado.classList.add("colorPlayer1");
-            bntClicado.innerHTML = " X ";            
-            jogadorDaVez(player2)            
-            playerQueJogou = jogadorDaVez(player2)
-            i++
-            
+                bntClicado.classList.add("colorPlayer1");
+                bntClicado.innerHTML = " X ";            
+                jogadorDaVez(player2)            
+                playerQueJogou = jogadorDaVez(player2)
+                i++
 
-        } else if (playerQueJogou === player2){  
-            bntClicado.classList.add("colorPlayer2");
-            bntClicado.innerHTML = " O ";              
-            jogadorDaVez(player1)           
-            playerQueJogou = jogadorDaVez(player1)
-            i++
-            
-
-        }
-        bntClicado.setAttribute('disabled','true'); 
-        
-        derterminarVencedor(player1, player2)
-        }
-        )
+            } else if (playerQueJogou === player2){  
+                bntClicado.classList.add("colorPlayer2");
+                bntClicado.innerHTML = " O ";              
+                jogadorDaVez(player1)           
+                playerQueJogou = jogadorDaVez(player1)
+                i++
+            }
+            bntClicado.setAttribute('disabled','true'); 
+            derterminarVencedor(player1, player2)
+        })
     })
-    
 }
-
-//criar uma função para determinar vencedor se baseando no valores dos botões, buscar pelos valores dos botões da tabela e a partir desses valores validar com um if ou switch as condições.
-
 function derterminarVencedor (player1, player2){
-
     const dadosTabela = [];
     const bnts = document.querySelectorAll('.bnt');
     bnts.forEach(function(bnt){
             dadosTabela.push(bnt.innerHTML);
     });
     const vencedor = document.querySelector("#vencedor"); 
-
     // Para vitórias em linhas
     for ( a = 0; a < dadosTabela.length; a += 3)
     {
@@ -127,20 +101,20 @@ function derterminarVencedor (player1, player2){
         const areaDestacada1 = document.querySelector(`#areaDoVencedor-${a}`)
         const areaDestacada2 = document.querySelector(`#areaDoVencedor-${b}`)
         const areaDestacada3 = document.querySelector(`#areaDoVencedor-${c}`);
-    if (dadosTabela[a] === " X " && dadosTabela[b] === " X " && dadosTabela[c] === " X ")
-    {
-        vencedor.value = player1;  
-        areaDestacada1.classList.add("vencedorPlayer1");
-        areaDestacada2.classList.add("vencedorPlayer1");
-        areaDestacada3.classList.add("vencedorPlayer1");
-    } 
-    else if (dadosTabela[a] === " O " && dadosTabela[b] === " O " && dadosTabela[c] === " O ")
-    {
-        vencedor.value = player2;
-        areaDestacada1.classList.add("vencedorPlayer2");
-        areaDestacada2.classList.add("vencedorPlayer2");
-        areaDestacada3.classList.add("vencedorPlayer2");
-    }   
+        if (dadosTabela[a] === " X " && dadosTabela[b] === " X " && dadosTabela[c] === " X ")
+        {
+            vencedor.value = player1;  
+            areaDestacada1.classList.add("vencedorPlayer1");
+            areaDestacada2.classList.add("vencedorPlayer1");
+            areaDestacada3.classList.add("vencedorPlayer1");
+        } 
+        else if (dadosTabela[a] === " O " && dadosTabela[b] === " O " && dadosTabela[c] === " O ")
+        {
+            vencedor.value = player2;
+            areaDestacada1.classList.add("vencedorPlayer2");
+            areaDestacada2.classList.add("vencedorPlayer2");
+            areaDestacada3.classList.add("vencedorPlayer2");
+        }   
     }
     // Para vitórias em colunas  
     for ( a = 0; a < dadosTabela.length; a++)
@@ -153,14 +127,14 @@ function derterminarVencedor (player1, player2){
 
         if (dadosTabela[a] === " X " && dadosTabela[b] === " X " && dadosTabela[c] === " X ")
         {       
-            vencedor.value =`${player1} Venceu a partida!`;  
+            vencedor.value =player1;  
             areaDestacada1.classList.add("vencedorPlayer1");
             areaDestacada2.classList.add("vencedorPlayer1");
             areaDestacada3.classList.add("vencedorPlayer1");
         } 
         else if (dadosTabela[a] === " O " && dadosTabela[b] === " O " && dadosTabela[c] === " O ")
         {
-            vencedor.value =`${player2} Venceu a partida!`; 
+            vencedor.value = player2; 
             areaDestacada1.classList.add("vencedorPlayer2");
             areaDestacada2.classList.add("vencedorPlayer2");
             areaDestacada3.classList.add("vencedorPlayer2")
@@ -172,118 +146,49 @@ function derterminarVencedor (player1, player2){
         a != 2 ?  a = 0 : a = 2;
         b = 4;
         c != 8 ?  c = 8 : c = 6;
-
         const areaDestacada1 = document.querySelector(`#areaDoVencedor-${a}`)
         const areaDestacada2 = document.querySelector(`#areaDoVencedor-${b}`)
         const areaDestacada3 = document.querySelector(`#areaDoVencedor-${c}`);
-
         if (dadosTabela[a] === " X " && dadosTabela[b] === " X " && dadosTabela[c] === " X ")
         {
-            vencedor.value =`${player1} Venceu a partida!`;  
+            vencedor.value = player1;  
             areaDestacada1.classList.add("vencedorPlayer1");
             areaDestacada2.classList.add("vencedorPlayer1");
             areaDestacada3.classList.add("vencedorPlayer1");
             bloquearTabela(player1)
-        
         } 
         else if (dadosTabela[a] === " O " && dadosTabela[b] === " O " && dadosTabela[c] === " O ")
         {
-            vencedor.value =`${player2} Venceu a partida!`;  
+            vencedor.value = player2;  
             areaDestacada1.classList.add("vencedorPlayer2");
             areaDestacada2.classList.add("vencedorPlayer2");
             areaDestacada3.classList.add("vencedorPlayer2");
             bloquearTabela(player2)
-
         }  
     }    
-
+    // Bloqueia tabela se houver vencedor
     if(vencedor.value === player2 ||  vencedor.value === player1)
     {
-        bloquearTabela(dadosTabela)
+        bloquearTabela(dadosTabela);
     }
+    // Declara empate e bloqueia a tabela
+    const bntsRestantesVazios = dadosTabela.filter(function(bntVazio)
+        {
+        return  bntVazio ===  '';}
+    ).length;
+    console.log(bntsRestantesVazios);
 
-
-    // Para empates
-    if (dadosTabela.length === 9 && vencedor.value === "")
+    if (bntsRestantesVazios === 2 && vencedor.value === "")
     {
         vencedor.value = "Empate";
+        bloquearTabela()
     }
-
-
 }
 // Bloquea a tabela quando um jogador vencer
     function bloquearTabela (dadosTabela)
     {
-            const myDados = dadosTabela.filter(function(dados){
-                return  dados ===  ''
-                
-            });
-            
-    
-            //document.querySelectorAll(".bnt").innerHTML = dados;
+           const block = document.querySelectorAll(".bnt");
+            block.forEach(function(bnt){
+                bnt.setAttribute('disabled','true');
+            })
     }
-/*
-})
-    
-     if (dadosTabela[3] === " X " && dadosTabela[4] === " X " && dadosTabela[5] === " X " )
-    {       
-        vencedor.value = `${player1} Venceu a partida!`;
-    }
-    else if (dadosTabela[6] === " X " && dadosTabela[7] === " X " && dadosTabela[8] === " X " )
-    {       
-        vencedor.value = `${player1} Venceu a partida!`;
-    } 
-    else if (dadosTabela[0] === " X " && dadosTabela[3] === " X " && dadosTabela[6] === " X " )
-    {       
-        vencedor.value = `${player1} Venceu a partida!`;
-    }
-     else if (dadosTabela[1] === " X " && dadosTabela[4] === " X " && dadosTabela[7] === " X " )
-    {       
-        vencedor.value = `${player1} Venceu a partida!`;
-    } 
-    else if (dadosTabela[2] === " X " && dadosTabela[5] === " X " && dadosTabela[8] === " X " )
-    {       
-        vencedor.value = `${player1} Venceu a partida!`;
-    } 
-    else if (dadosTabela[0] === " X " && dadosTabela[4] === " X " && dadosTabela[8] === " X " )
-    {       
-        vencedor.value = `${player1} Venceu a partida!`;
-    } 
-    else if (dadosTabela[2] === " X " && dadosTabela[4] === " X " && dadosTabela[6] === " X " )
-    {       
-        vencedor.value = `${player1} Venceu a partida!`;
-    } 
-
-     if (dadosTabela[0] === " O " && dadosTabela[1] === " O " && dadosTabela[2] === " O " )
-    {
-        vencedor.value = `${player2} Venceu a partida!`;
-    }
-    else if (dadosTabela[3] === " O " && dadosTabela[4] === " O " && dadosTabela[5] === " O " )
-    {       
-        vencedor.value = `${player2} Venceu a partida!`;
-    } 
-    else if (dadosTabela[6] === " O " && dadosTabela[7] === " O " && dadosTabela[8] === " O " )
-    {
-        vencedor.value = `${player2} Venceu a partida!`;
-    }
-    else if (dadosTabela[0] === " O " && dadosTabela[3] === " O " && dadosTabela[6] === " O " )
-    {       
-        vencedor.value = `${player2} Venceu a partida!`;
-    }
-     else if (dadosTabela[1] === " O " && dadosTabela[4] === " O " && dadosTabela[7] === " O " )
-    {       
-        vencedor.value = `${player2} Venceu a partida!`;
-    } 
-    else if (dadosTabela[2] === " O " && dadosTabela[5] === " O " && dadosTabela[8] === " O " )
-    {       
-        vencedor.value = `${player2} Venceu a partida!`;
-    } 
-    else if (dadosTabela[0] === " O " && dadosTabela[4] === " O " && dadosTabela[8] === " O " )
-    {       
-        vencedor.value = `${player2} Venceu a partida!`;
-    } 
-    else if (dadosTabela[2] === " O " && dadosTabela[4] === " O " && dadosTabela[6] === " O " )
-    {       
-        vencedor.value = `${player2} Venceu a partida!`;
-    }
-*/
